ce3086b0c5f7acefb1c560516016caecaeebb09d
Arnau Mora <arnyminer.z@gmail.com>
2022-10-19 21:12:20 +0200

    Assertions

--- VerificameEsta/tb_divisor_parallel.sv
+++ VerificameEsta/tb_divisor_parallel.sv
@@ -1,5 +1,5 @@
 module tb_divisor_parallel #(
-    parameter integer tamanyo = 16
+    parameter integer tamanyo = 32
 )();
 logic RSTa, Start, Done, CLK;
 logic [tamanyo-1:0] Num, Den, Coc, Res;
@@ -20,81 +20,98 @@ Divisor_Algoritmico #(
 
 initial
 begin
+    $display("Ejecutando tests para", tamanyo, "bits");
+
     RSTa=1'b0;
-	 CLK=1'b0;
+	CLK=1'b0;
     Start=1'b0;
-    Num=16'b0;
-    Den=16'b0;
+    Num='b0;
+    Den='b0;
     #(T*2);
     RSTa=1'b1;
 
     // Ambos operandos positivos sin resto
-    Num=16'd15;
-    Den=16'd3;
-    #(T);
-    Start=1'b1;
-    @(posedge Done);
-    Start=1'b0; 
-
-    // Ambos operandos positivos con resto
-    Num=16'd17;
-    Den=16'd3;
+    Num='d15;
+    Den='d3;
     #(T);
     Start=1'b1;
     @(posedge Done);
     Start=1'b0;
+    assert (Coc == 5) else $error("El cociente deberia ser 5");
+    assert (Res == 0) else $error("El residuo deberia ser 0");
+
+    // Ambos operandos positivos con resto
+    Num='d17;
+    Den='d3;
+    #(T);
+    Start=1'b1;
+    @(posedge Done);
+    Start=1'b0;
+    assert (Coc == 5) else $error("El cociente deberia ser 5");
+    assert (Res == 2) else $error("El residuo deberia ser 2");
 
 
     // Numerador positivo y Denominador negativo sin resto
-    Num = 16'd15;
-    Den = -16'd3;
+    Num = 'd15;
+    Den = -'d3;
     #(T);
     Start = 1'b1;
     @(posedge Done);
     Start = 1'b0;
+    assert (Coc == -5) else $error("El cociente deberia ser -5");
+    assert (Res == 0) else $error("El residuo deberia ser 0");
 
     // Numerador positivo y Denominador negativo con resto
-    Num = 16'd17;
-    Den = -16'd3;
+    Num = 'd17;
+    Den = -'d3;
     #(T);
     Start = 1'b1;
     @(posedge Done);
     Start = 1'b0;
+    assert (Coc == -5) else $error("El cociente deberia ser -5");
+    assert (Res == 2) else $error("El residuo deberia ser 2");
 
 
     // Ambos operandos negativos sin resto
-    Num=-16'd15;
-    Den=-16'd3;
+    Num=-'d15;
+    Den=-'d3;
     #(T);
     Start=1'b1;
     @(posedge Done);
     Start=1'b0; 
+    assert (Coc == 5) else $error("El cociente deberia ser 5");
+    assert (Res == 0) else $error("El residuo deberia ser 0");
 
     // Ambos operandos negativos con resto
-    Num=-16'd23;
-    Den=-16'd5;
+    Num=-'d23;
+    Den=-'d5;
     #(T);
     Start=1'b1;
     @(posedge Done);
     Start=1'b0;  
+    assert (Coc == 4) else $error("El cociente deberia ser 4");
+    assert (Res == -3) else $error("El residuo deberia ser -3");
 
     // Numerador negativo y Denominador positivo con resto
-
-    Num = -16'd17;
-    Den = 16'd3;
+    Num = -'d17;
+    Den = 'd3;
     #(T);
     Start = 1'b1;
     @(posedge Done);
     Start = 1'b0;
+    assert (Coc == -5) else $error("El cociente deberia ser -5");
+    assert (Res == -2) else $error("El residuo deberia ser -2");
 
     // Numerador negativo y Denominador positivo sin resto
 
-    Num = -16'd18;
-    Den = 16'd3;
+    Num = -'d18;
+    Den = 'd3;
     #(T);
     Start = 1'b1;
     @(posedge Done);
     Start = 1'b0; 
+    assert (Coc == -6) else $error("El cociente deberia ser -6");
+    assert (Res == 0) else $error("El residuo deberia ser 0");
 
 	$stop;
 end
