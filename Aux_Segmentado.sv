201f282b6795b5294af585cf6cf8791bc2833521
Carlos Villena <carlosviji10@gmail.com>
2022-11-14 12:44:36 +0100

    TODO COMENTADO

--- Aux_Segmentado.sv
+++ Aux_Segmentado.sv
@@ -1,32 +1,36 @@
+/*
+    Declaramos el módulo auxiliar que realiza los módulos desde el primero hasta el justo último de los estados
+    que vayamos a implementar según el número de bits que se vayan a asignar (tamaño del array)
+*/
 module Aux_Segmentado (
     // ! Entradas ! \\
-    input logic CLK, RSTa, Start,
-    input logic SignNum, SignDen,
-    input logic [`LAST_BIT:0] Q, M, ACCU,
+    input logic CLK, RSTa, Start,                   // Declaramos las variables lógicas de reloj, reset y comienzo de la operación de división
+    input logic SignNum, SignDen,                   // Declaramos las variables de único bit de los signos del Denominador(Den) y Numerador(Num) {0-->Positivo ; 1-->Negativo}
+    input logic [`LAST_BIT:0] Q, M, ACCU,           // Declaramos las variables (Q,M,ACCU) de tamaño a elegir con la definición "LAST_BIT"
 
     // ! Salidas ! \\
-    output logic [`LAST_BIT:0] Q_out, M_out, ACCU_out,
-    output logic SignNum_out, SignDen_out,
-    output logic Done
+    output logic [`LAST_BIT:0] Q_out, M_out, ACCU_out,  // Declaramos las variables de salidas de (Q,M,ACCU) de igual tamaño a las entradas
+    output logic SignNum_out, SignDen_out,              // Declaramos las variables de salida de los signos del numerador y denominador
+    output logic Done                                   // Declaramos la variables de salida(Done) que actúa como valor de Start a partir del módulo 1 en adelante
 );
 
-logic [`LAST_BIT:0] ACCU_int, Q_int;
+logic [`LAST_BIT:0] ACCU_int, Q_int;        // Declaramos los registros internos del acumulador(ACCU) y del cociente(Q), siempre de igual tamaño
 
 assign {ACCU_int, Q_int} = {ACCU[`BIT_SIZE-2:0],Q,1'b0};
 
 always_ff @(posedge CLK, negedge RSTa) begin
     // Para reiniciar el módulo, borramos el valor de todas las salidas
     if (!RSTa)
-        {Q_out, M_out, ACCU_out, SignNum_out, SignDen_out, Done} = '0;
-    else begin
-        M_out <= M;
-        SignNum_out <= SignNum;
-        SignDen_out <= SignDen;
+        {Q_out, M_out, ACCU_out, SignNum_out, SignDen_out, Done} = '0;    // Llenamos a cero todas las variables si se activa la señal de RESET
+    else begin      // Si no se pulsa el reset y el reloj Actúa -->
+        M_out <= M;                 // El valor del resto de la división queda registrada en la salida(M_out)
+        SignNum_out <= SignNum;     // Las salidas de los signos cogen ahora los valores previos/iniciales que tenían
+        SignDen_out <= SignDen;     //
 
-        Q_out <= (ACCU_int >= M) ? Q_int + 1 : Q_int;
-        ACCU_out <= (ACCU_int >= M) ? ACCU_int-M : ACCU_int;
+        Q_out <= (ACCU_int >= M) ? Q_int + 1 : Q_int;   // Si (ACCU_int>=M) se le asignará a la salida de Q(Q_out) un valor más al valor de Q_interno, y si la condición no se cumple Q_out pillará el valor interno actual de Q sin aumentar nada.
+        ACCU_out <= (ACCU_int >= M) ? ACCU_int-M : ACCU_int;    // Si (ACCU_int>=M) le asignamos a la salida del acumulador(ACCU_out) justo el calor interno del acumulador menos el valor del resto, sino se cumple dicha condición coge el valor interno del acumulador.
 
-        Done <= Start;
+        Done <= Start;      // Guardamos en Done la señal de start, y ahora Done actuará como el propio Start
     end
 end
 
--- Dividor_Segmentado.sv
+++ Dividor_Segmentado.sv
@@ -24,8 +24,8 @@ module Dividor_Segmentado (
 
 localparam etapas=`BIT_SIZE; //2**`BIT_SIZE;
 
-logic [etapas-1:0][`LAST_BIT:0] ACCU, Q, M;
-logic [etapas-1:0] SignNum, SignDen, Done_mem;
+logic [etapas-1:0][`LAST_BIT:0] ACCU, Q, M;  // Declaramos el acumulador, el contador del cociente y el del resto
+logic [etapas-1:0] SignNum, SignDen, Done_mem;  // Declaramos los array del signo del numerador, denominador y el estado de paso realizado en memoria
 
 /* 
    Queremos realizar un Divisor Segmentado, es decir un divisor algoritmico pero que realize dicha 
@@ -35,7 +35,7 @@ logic [etapas-1:0] SignNum, SignDen, Done_mem;
    la operación se haga en dicho golpe de reloj cada paso hasta comletar el resultado completo de la división.
 
    Para ello, también implementamos la función generate que ayuda  a la implementación de varias acciones de forma única.
-   Es ahí donde atacaremos con lso bucles 'for'.
+   Es ahí donde atacaremos con los bucles 'for'.
 
 */
 
@@ -49,25 +49,25 @@ generate
          // 30 módulos restantes ya que estos dependen dirécatemente del módulo anterior y operan de igual forma
          // (Los únicos módulos en los que se procede de forma diferente son en el primero(0) y úlimo(`BIT_SIZE) ).
 
-         case(i)
-
-            0:    // Módulo en cuanto la cuenta está sin empezar y justo se Activa la señal de cmoienzo
+         case(i)  // En caso de estar en módulo 'i' se realiza las siguientes acciones.
+ 
+            0:    // Módulo en cuanto la cuenta está sin empezar y justo se Activa la señal de cmoienzo.
                Aux_Segmentado Comienzo_Division (     // Declaramos la primera instancia del divisor auxiliar que permite
-                                                                           // iniciar la cadena de sumadores
-                  .CLK(CLK),     // Conectamos el CLK de la instancia al del módulo
-                  .RSTa(RSTa),   // Conectamos el RSTa de la instancia al del módulo
-                  .Start(Start), // Conectamos el Start de la instancia al del módulo
-                  .SignNum(Num[`LAST_BIT]),
-                  .SignDen(Den[`LAST_BIT]),
-                  .ACCU('0),
-                  .Q(Num[`LAST_BIT] ? (~Num+1) : Num),
-                  .M(Den[`LAST_BIT] ? (~Den+1) : Den),
-                  .ACCU_out(ACCU[i]),
-                  .Q_out(Q[i]),
-                  .M_out(M[i]),
-                  .SignNum_out(SignNum[i]),
+                                                                           // iniciar la cadena de sumadores.
+                  .CLK(CLK),                             // Conectamos el CLK de la instancia al del módulo.
+                  .RSTa(RSTa),                           // Conectamos el RSTa de la instancia al del módulo.
+                  .Start(Start),                         // Conectamos el Start de la instancia al del módulo.
+                  .SignNum(Num[`LAST_BIT]),              // Conectamos SignNum con el valor de Num y solo buscamos para el bit más significativo , es decir, el que nos determina si es un valor positivo (0) 0 negativo (1).
+                  .SignDen(Den[`LAST_BIT]),              // De igaul forma que la anterior pero para el signo del denominador .
+                  .ACCU('0),                             // El acumulador al principio está a cero total ya que no se ha realizado de momento ninguna acción, pero si que cambiará el valor para la salida del propio acumulador.
+                  .Q(Num[`LAST_BIT] ? (~Num+1) : Num),   //
+                  .M(Den[`LAST_BIT] ? (~Den+1) : Den),   //
+                  .ACCU_out(ACCU[i]),                    // La salida del ACCU está conectada con el valor que tenga de entrada para el estado en el que se encuentre, que en este caso (i=0)
+                  .Q_out(Q[i]),                          // Q_out coge la salida de Q a instancia de (i = 0)
+                  .M_out(M[i]),                          // M_out se conecta a la propia entrada en el estado inicial (i = 0)
+                  .SignNum_out(SignNum[i]),              // La salida del signo en Num y Den coge los valor de las entradas para (i = 0)
                   .SignDen_out(SignDen[i]),
-                  .Done(Done_mem[i])
+                  .Done(Done_mem[i])                     // El valor que se guarda en Done va ha ser el valor que tenga asignado a primer momento (i = 0)
                );
 
             etapas:    // Módulo en cuanto la cuenta llega a su fin y se completa la división, aquí asignamos el valor final -->
@@ -83,33 +83,33 @@ generate
                      begin 
                         Coc <= (SignNum[i-1]^SignDen[i-1]) ? (~Q[i-1]+1) : Q[i-1];
                         Res <= (SignNum[i-1]) ? (~ACCU[i-1]+1) : ACCU[i-1];
-                        Done <= Done_mem[i-1];
+                        Done <= Done_mem[i-1];        // Actualizamos el valor del start con el valor que se haya guardado en el último módulo realizado en el default.
                      end
                end
 
-            default:
+            default:    // Con el default entramos en cualquier módulo diferente del inicial(0) y el final(etapas)
                Aux_Segmentado Siguiendo_Division (     // Declaramos la segunda instancia del divisor auxiliar que permite
                                                                            // continuar la cadena de sumadores desde i=1 hasta i=31.
-                  .CLK(CLK),     // Conectamos el CLK de la instancia al del módulo
-                  .RSTa(RSTa),   // Conectamos el RSTa de la instancia al del módulo
-                  .Start(Done_mem[i-1]),  // Conectamos el Start de la instancia al ---------
-                  .SignNum(SignNum[i-1]),
-                  .SignDen(SignDen[i-1]),
-                  .ACCU(ACCU[i-1]),
-                  .Q(Q[i-1]),
-                  .M(M[i-1]),
-                  .ACCU_out(ACCU[i]),
-                  .Q_out(Q[i]),
-                  .M_out(M[i]),
-                  .SignNum_out(SignNum[i]),
-                  .SignDen_out(SignDen[i]),
-                  .Done(Done_mem[i])
+                  .CLK(CLK),                    // Conectamos el CLK de la instancia al del módulo
+                  .RSTa(RSTa),                  // Conectamos el RSTa de la instancia al del módulo
+                  .Start(Done_mem[i-1]),        // Conectamos el Start de la instancia al valor anterior asignado en Done_mem (ya que Done_mem actúa como start para los demás procesos que no sean el inicial)
+                  .SignNum(SignNum[i-1]),       // Como estamos en los módulo comprendidos en [1,LAST_BIT], el valor de signo actual será otorgado por su justo inmediato anterior
+                  .SignDen(SignDen[i-1]),       // Igual ocurrirá con el signo del denominador
+                  .ACCU(ACCU[i-1]),             // Igual ocurre con el bit del acumulador
+                  .Q(Q[i-1]),                   // Guardamos  Q como su valor previo
+                  .M(M[i-1]),                   // Guardamos  M como su valor previo
+                  .ACCU_out(ACCU[i]),           // En cambio, para la salida se guardará el que justo vayamos a utilizar para el siguiente módulo
+                  .Q_out(Q[i]),                 // La salida del registro Q se le asignará el valor justo que tenga en ese preciso módulo (Que fue asignado en el módulo justo anterior)
+                  .M_out(M[i]),                 // Lo mismo ocurre con la salida del registro M
+                  .SignNum_out(SignNum[i]),     //
+                  .SignDen_out(SignDen[i]),     //   
+                  .Done(Done_mem[i])            // Guardamos el último valor de lo que haya guardado en Done_mem
 
                );
 
 
-         endcase
+         endcase     // Terminamos el bucle case/default
       end
-endgenerate
+endgenerate          // Terminamos la función generate
 
 endmodule 
